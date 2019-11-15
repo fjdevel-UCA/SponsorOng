c340d186cfcf9ed12da1315bdf3b629225a2748b
marielosagui <00078913@uca.edu.sv>
2019-11-14 21:29:29 -0600

    FIX: login donante

--- besponsor/src/components/login.js
+++ besponsor/src/components/login.js
@@ -11,27 +11,53 @@ export default class login extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <h3>LOGIN</h3>
-                    <hr />
-                    <form action="" method="post">
-                        <div>
-                            <label for="name">User:</label>
-                            <input type="text" id="name" name="nombre_donante" />
-                        </div>
-                        <div>
-                            <label for="org">Password:</label>
-                            <input type="text" id="org" name="org_financiar" />
-                        </div>
+            /* <div class="abs-center col-md-6 col-md-offset-3">
+                <div class="card card-box card-danger">
+                    <div class="card-header ">
+                        <h2>Login</h2>
+                    </div>
+                    <img src=""></img>
+                    <div class="brand-container"><i class="fas fa-hand-holding-usd"></i></div>
+                    <div class="card-body">
+                        <form class="form-horizontal">
+                            <div class="form-group has-error">
+                                <label for="n-email">Email address</label><input type="email" class="form-control" id="n-email" aria-describedby="email" placeholder="Enter email"></input> <span class="form-text">Please enter valid email.</span>
+                            </div>
 
-                        <div class="button">
-                            <button type="submit">login </button>
-                        </div>
-                    </form>
+                                <div class="form-group"><label for="n-password">Password</label><input type="password" class="form-control" id="n-password" placeholder="Password"></input>
+                                </div> */
+                <main role="main" class="container my-auto">
+            <div class="row">
+            <div id="login" class="col-lg-4 offset-lg-4 col-md-10 offset-md-3 col-12">
+                <h2 class="text-center">Bienvenido</h2>
+                <img class="img-fluid mx-auto d-block rounded"
+                    src="http://emser.es/wp-content/uploads/2016/08/usuario-sin-foto.png" width="100px" />
+
+                <form>
+                    <div class="form-group">
+                        <label for="correo">Correo</label>
+                        <input id="correo" name="correo"
+                            class="form-control" type="email"
+                            placeholder="Correo electrónico"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="palabraSecreta">Contraseña</label>
+                        <input id="palabraSecreta" name="palabraSecreta"
+                            class="form-control" type="password"
+                            placeholder="Contraseña"></input>
+                    </div>
+
+                    <div class="btn btn-default">
+                        <button type="submit" class="btn btn-danger">Iniciar Sesion</button>
+                    </div>
+                    <div class="btn btn-default">
+                        <button type="submit" class="btn btn-danger">Register</button>
+                    </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        );
-    }
 
+            </main>
+ );
+                    }
 }
