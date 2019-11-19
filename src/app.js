04418f7cb394d96506fb3bc8ddc7392afe77ea8d
Frederick Mejia <00404919@uca.edu.sv>
2019-11-19 00:58:24 -0600

    autenticacion terminada, falta obtener las rutas permitidas

--- src/app.js
+++ src/app.js
@@ -8,7 +8,25 @@ import securityRoutes from './routes/security';
 
 //initialization
 const app = express();
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 //middlewares
 app.use(morgan('dev'));
@@ -18,6 +36,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
+
 
 //Routes
 app.use("/api/security",securityRoutes);
--- src/controllers/security.controller.js
+++ src/controllers/security.controller.js
@@ -1,31 +1,34 @@
 import Usuario from "../models/Usuario";
 import isLoggedIn from "../middlewares/securityMiddleware";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const saltRounds = 12;
 
 export function test(req, res) {
-  console.log(req.body)
-  res.send("dato recibido")
+  console.log(req.body);
+  res.send("dato recibido");
 }
 
 export function login(req, res) {
   if (req.body.username === "" || req.body.password === "") {
-    res.send("login failed")
+    res.send("login failed");
   } else {
-      const usuario = req.body.username
-      const pass = req.body.password
-    Usuario.findOne({where: {nombreusuario: usuario }}).then(user=>{
-        if(!user)
-            res.send(400)
-        bcrypt.compare(pass,user.password,(err,data)=>{
-            if(err) throw err
-            if(data){
-                res.send(200)
-            }else{
-                res.send(401);
-            }
-        })
+    const usuario = req.body.username;
+    const pass = req.body.password;
+    Usuario.findOne({ where: { nombreusuario: usuario } }).then(user => {
+      if (!user) {
+        res.status(400).send("verifique el usuario o la contraseña");
+      } else {
+        bcrypt.compare(pass, user.password, (err, data) => {
+          if (err) throw err;
+          if (data) {
+            
+            res.status(200).send({'redirect':'/'});
+          } else {
+            res.status(401).send("verifique el usuario o la contraseña");
+          }
+        });
+      }
     });
   }
 }
