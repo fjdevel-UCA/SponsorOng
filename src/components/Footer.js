80e3e19c161f5f95cf86e3f4f9bbc562cb742cd7
caceres06 <37302560+caceres06@users.noreply.github.com>
2019-11-27 23:24:36 -0600

    pequeños cambios al footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render(){
         return(
             <div className="footer">
-                    footer
+                    Todos los derechos reservados.
             </div>
         );
     }
