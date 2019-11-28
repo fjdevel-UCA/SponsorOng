575702dc52117bf15e23f13dfa3f5fb012fe715e
00086116 <00086116@uca.edu.sv>
2019-11-27 18:56:22 -0600

    Vista Error 404: terminada

--- src/views/Error404.js
+++ src/views/Error404.js
@@ -2,6 +2,19 @@ import React from "react";
 import styles from "./CSS/Error.css"
 export default class Error404 extends React.Component {
     render(){
-        return(<h1>Error 404</h1>)
+        return(
+             <div id="clouds">
+            <div class="cloud x1"></div>
+            <div class="cloud x1_5"></div>
+            <div class="cloud x2"></div>
+            <div class="cloud x3"></div>
+            <div class="cloud x4"></div>
+            <div class="cloud x5"></div>
+            <div class='_404'>404</div>
+            <div class='_1'>THE PAGE</div>
+            <div class='_2'>WAS NOT FOUND</div>
+            <a class='btn1' href='#'>BACK TO HOME</a>
+        </div>
+        )
     }
 }
